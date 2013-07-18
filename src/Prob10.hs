55ddba8044aa602e78932174a7b8a55718c5d566
Hyrulia <Hyrulia@live.fr>
2013-07-18 17:49:18 +0100

    Cleaning

--- src/Prob10.hs
+++ src/Prob10.hs
@@ -6,10 +6,10 @@ isPrime n
         | n < 2 = False
         | n == 2 = True
         | mod n 2 == 0 = False
-        | otherwise = not $ any divisible numbers 
+        | otherwise = not $ any divisible [3, 5 .. i]
         where 
-                divisible = \x -> mod n x == 0
-                numbers = takeWhile (\x -> x <= p) [3, 5 .. p]
-                p = floor $ sqrt $ fromIntegral n
-                
-prob = sum $ filter isPrime [2..2000000]
+                divisible x = mod n x == 0              
+                i = floor $ sqrt (fromIntegral n :: Double)
+      
+main :: Int                
+main = sum $ filter isPrime [2..2000000]
